a3c66c8574be45bf106f27bdfe29f4cdb6692a1e
hlong19it3 <hlong.19it3@vku.udn.vn>
2022-10-15 19:33:15 +0700

    create refreshToken

--- backend/src/app/controllers/api/LoginController.js
+++ backend/src/app/controllers/api/LoginController.js
@@ -1,6 +1,38 @@
 const { sequelize, User } = require("../../database/models");
 const { sign } = require("jsonwebtoken");
 
+// const generateToken = (payload) => {
+//   const accessToken = sign(payload, "shhhh", {
+//     expiresIn: "20s",
+//   });
+//   const refreshToken = sign(payload, "refresh", {
+//     expiresIn: "2 days",
+//   });
+
+//   return { accessToken, refreshToken };
+// };
+
+const updateRefreshToken = async (email, refreshToken) => {
+  const userCollection = await User.findAll({
+    where: {
+      email: `${email}`,
+      // password: `${password}`,
+    },
+  });
+  console.log(userCollection);
+  if (userCollection && userCollection.length > 0) {
+    console.log("da vo");
+    User.update(
+      { refresh_token: refreshToken },
+      {
+        where: {
+          email: email,
+        },
+      }
+    );
+  }
+  return userCollection;
+};
 class LoginController {
   async login(req, res) {
     const { email, password } = req.body;
@@ -15,12 +47,21 @@ class LoginController {
     //login
     if (userCollection && userCollection.length > 0) {
       userCollection.password = undefined;
+
+      // const tokens = generateToken(userCollection);
       const accessToken = sign({ userCollection: userCollection }, "shhhh", {
+        expiresIn: "20s",
+      });
+      const refreshToken = sign({ userCollection: userCollection }, "refresh", {
         expiresIn: "2 days",
       });
-      return res
-        .status(200)
-        .json({ msg: "Login successfully", token: accessToken });
+      updateRefreshToken(email, refreshToken);
+      console.log("hmm");
+      return res.status(200).json({
+        msg: "Login successfully",
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      });
     }
     res.status(400).json({ msg: "Login failed!" });
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,31 +1,34 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const handlebar = require("express-handlebars")
-const port = 3000
+const path = require("path");
+const express = require("express");
+const app = express();
+const handlebar = require("express-handlebars");
+const port = 3000;
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // views engine
-app.engine('.hbs', handlebar.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, './views'))
-
+app.engine(
+  ".hbs",
+  handlebar.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "./views"));
 
 //routes
-const route = require('./routes/route');
+const route = require("./routes/route");
 route(app);
-
 
 // run
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
