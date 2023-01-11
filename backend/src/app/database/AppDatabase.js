a280be232ddb4213b7ab4f81c8a7afa096e1d8be
hlong19it3 <hlong.19it3@vku.udn.vn>
2023-01-11 18:14:03 +0700

    test

--- backend/src/app/database/AppDatabase.js
+++ backend/src/app/database/AppDatabase.js
@@ -1,15 +1,21 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize('foodapp', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     operatorsAliases: 0,
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
