4734692e9ba5c5ba56321b59fe0a91589eaed6ae
hlong19it3 <hlong.19it3@vku.udn.vn>
2023-01-11 19:51:42 +0700

    test

--- src/app/database/models.js
+++ src/app/database/models.js
@@ -1,432 +1,431 @@
-const { Sequelize, DataTypes, Model } = require("sequelize");
-const sequelize = require("./AppDatabase");
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const sequelize = require('./AppDatabase')
 
-//test
 const Address = sequelize.define(
-  "address",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    address: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'address',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		address: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const Category = sequelize.define(
-  "category",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'category',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+		image: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const Food = sequelize.define(
-  "foods",
-  {
-    // Model attributes are defined here
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    restaurantId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    details: {
-      type: DataTypes.TEXT,
-      defaultValue: "",
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    sold: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    startSell: {
-      type: DataTypes.TIME,
-    },
-    endSell: {
-      type: DataTypes.TIME,
-    },
-    categoryId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    rating: {
-      type: DataTypes.DOUBLE,
-      defaultValue: 5,
-    },
-    totalRating: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'foods',
+	{
+		// Model attributes are defined here
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		restaurantId: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		details: {
+			type: DataTypes.TEXT,
+			defaultValue: '',
+		},
+		image: {
+			type: DataTypes.STRING,
+		},
+		quantity: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+		},
+		sold: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+		},
+		startSell: {
+			type: DataTypes.TIME,
+		},
+		endSell: {
+			type: DataTypes.TIME,
+		},
+		categoryId: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		rating: {
+			type: DataTypes.DOUBLE,
+			defaultValue: 5,
+		},
+		totalRating: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const Menu = sequelize.define(
-  "menu",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    foodId: {
-      type: DataTypes.STRING,
-      defaultValue: "",
-    },
-    restaurantId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.INTEGER,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'menu',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		foodId: {
+			type: DataTypes.STRING,
+			defaultValue: '',
+		},
+		restaurantId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.INTEGER,
+		},
+		description: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const OrderDetail = sequelize.define(
-  "orderDetail",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    foodId: {
-      type: DataTypes.INTEGER,
-    },
-    orderId: {
-      type: DataTypes.INTEGER,
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+	'orderDetail',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		foodId: {
+			type: DataTypes.INTEGER,
+		},
+		orderId: {
+			type: DataTypes.INTEGER,
+		},
+		quantity: {
+			type: DataTypes.INTEGER,
+		},
+		amount: {
+			type: DataTypes.INTEGER,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
 
 const Order = sequelize.define(
-  "order",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    customerId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    restaurantId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    orderDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    totalAmount: {
-      type: DataTypes.INTEGER,
-    },
-    orderStatus: {
-      type: DataTypes.INTEGER,
-    },
-    // createdAt: {
-    //   type: DataTypes.DATE,
-    //   defaultValue: DataTypes.NOW,
-    // },
-  },
-  {
-    timestamps: true,
-  }
-);
+	'order',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		customerId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		restaurantId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		orderDate: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		totalAmount: {
+			type: DataTypes.INTEGER,
+		},
+		orderStatus: {
+			type: DataTypes.INTEGER,
+		},
+		// createdAt: {
+		//   type: DataTypes.DATE,
+		//   defaultValue: DataTypes.NOW,
+		// },
+	},
+	{
+		timestamps: true,
+	}
+)
 
 const Rating = sequelize.define(
-  "rating",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    customerId: {
-      type: DataTypes.INTEGER,
-    },
-    orderId: {
-      type: DataTypes.INTEGER,
-    },
-    score: {
-      type: DataTypes.INTEGER,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'rating',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		customerId: {
+			type: DataTypes.INTEGER,
+		},
+		orderId: {
+			type: DataTypes.INTEGER,
+		},
+		score: {
+			type: DataTypes.INTEGER,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const Restaurant = sequelize.define(
-  "restaurant",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-    },
-    address: {
-      type: DataTypes.STRING,
-    },
-    rating: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'restaurant',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+		userId: {
+			type: DataTypes.INTEGER,
+		},
+		address: {
+			type: DataTypes.STRING,
+		},
+		rating: {
+			type: DataTypes.FLOAT,
+			defaultValue: 0,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 const User = sequelize.define(
-  "user",
-  {
-    // Model attributes are defined here
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    role: {
-      type: DataTypes.INTEGER,
-      defaultValue: 1,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    mainAddress: {
-      type: DataTypes.STRING,
-    },
-    birthday: {
-      type: DataTypes.DATEONLY,
-    },
-    gender: {
-      type: DataTypes.TINYINT,
-      defaultValue: 1,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      defaultValue: "",
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.TINYINT,
-      defaultValue: 0,
-    },
-    // registrationTime: {
-    //   type: DataTypes.DATE,
-    // },
-    lastAction: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    lat: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-    },
-    lg: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-    },
-    avatar: {
-      type: DataTypes.STRING,
-      defaultValue: "https://avatars.githubusercontent.com/u/56586682?v=4",
-    },
-    refreshToken: {
-      type: DataTypes.STRING(2000),
-      defaultValue: "",
-    },
-    // createdAt: {
-    //   type: DataTypes.DATE,
-    //   defaultValue: DataTypes.NOW,
-    // },
-  },
-  {
-    timestamps: true,
-  }
-);
+	'user',
+	{
+		// Model attributes are defined here
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		role: {
+			type: DataTypes.INTEGER,
+			defaultValue: 1,
+		},
+		firstName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		lastName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		mainAddress: {
+			type: DataTypes.STRING,
+		},
+		birthday: {
+			type: DataTypes.DATEONLY,
+		},
+		gender: {
+			type: DataTypes.TINYINT,
+			defaultValue: 1,
+		},
+		email: {
+			type: DataTypes.STRING,
+		},
+		phone: {
+			type: DataTypes.STRING,
+			defaultValue: '',
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		status: {
+			type: DataTypes.TINYINT,
+			defaultValue: 0,
+		},
+		// registrationTime: {
+		//   type: DataTypes.DATE,
+		// },
+		lastAction: {
+			type: DataTypes.DATE,
+			defaultValue: DataTypes.NOW,
+		},
+		lat: {
+			type: DataTypes.FLOAT,
+			defaultValue: 0,
+		},
+		lg: {
+			type: DataTypes.FLOAT,
+			defaultValue: 0,
+		},
+		avatar: {
+			type: DataTypes.STRING,
+			defaultValue: 'https://avatars.githubusercontent.com/u/56586682?v=4',
+		},
+		refreshToken: {
+			type: DataTypes.STRING(2000),
+			defaultValue: '',
+		},
+		// createdAt: {
+		//   type: DataTypes.DATE,
+		//   defaultValue: DataTypes.NOW,
+		// },
+	},
+	{
+		timestamps: true,
+	}
+)
 
 const Voucher = sequelize.define(
-  "voucher",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    restaurantId: {
-      type: DataTypes.INTEGER,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    type: {
-      type: DataTypes.TINYINT,
-    },
-    value: {
-      type: DataTypes.FLOAT,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+	'voucher',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		restaurantId: {
+			type: DataTypes.INTEGER,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+		type: {
+			type: DataTypes.TINYINT,
+		},
+		value: {
+			type: DataTypes.FLOAT,
+		},
+	},
+	{
+		timestamps: false,
+	}
+)
 
 User.hasOne(Address, {
-  foreignKey: "userId",
-});
+	foreignKey: 'userId',
+})
 
-Address.belongsTo(User);
+Address.belongsTo(User)
 
 User.hasMany(Order, {
-  foreignKey: "userId",
-});
+	foreignKey: 'userId',
+})
 
-Order.belongsTo(User);
+Order.belongsTo(User)
 
 User.hasOne(Restaurant, {
-  foreignKey: "userId",
-});
+	foreignKey: 'userId',
+})
 
-Restaurant.belongsTo(User);
+Restaurant.belongsTo(User)
 
 User.hasMany(Rating, {
-  foreignKey: "userId",
-});
+	foreignKey: 'userId',
+})
 
-Rating.belongsTo(User);
+Rating.belongsTo(User)
 
 Order.hasMany(OrderDetail, {
-  foreignKey: "orderId",
-});
+	foreignKey: 'orderId',
+})
 
-OrderDetail.belongsTo(Order);
+OrderDetail.belongsTo(Order)
 
 Order.hasOne(Rating, {
-  foreignKey: "orderId",
-});
+	foreignKey: 'orderId',
+})
 
-Rating.belongsTo(Order);
+Rating.belongsTo(Order)
 
 Food.hasMany(OrderDetail, {
-  foreignKey: "foodId",
-});
+	foreignKey: 'foodId',
+})
 
-OrderDetail.belongsTo(Food);
+OrderDetail.belongsTo(Food)
 
 Category.hasMany(Food, {
-  foreignKey: "categoryId",
-});
+	foreignKey: 'categoryId',
+})
 
-Food.belongsTo(Category);
+Food.belongsTo(Category)
 
 Restaurant.hasOne(Menu, {
-  foreignKey: "restaurantId",
-});
+	foreignKey: 'restaurantId',
+})
 
 Restaurant.hasMany(Food, {
-  foreignKey: "restaurantId",
-});
+	foreignKey: 'restaurantId',
+})
 
-Food.belongsTo(Restaurant);
+Food.belongsTo(Restaurant)
 
 Restaurant.hasMany(Order, {
-  foreignKey: "restaurantId",
-});
+	foreignKey: 'restaurantId',
+})
 
-Order.belongsTo(Restaurant);
+Order.belongsTo(Restaurant)
 
 Restaurant.hasMany(Voucher, {
-  foreignKey: "restaurantId",
-});
+	foreignKey: 'restaurantId',
+})
 
-Voucher.belongsTo(Restaurant);
+Voucher.belongsTo(Restaurant)
 
 // Sync all tables in database
 // sequelize.sync({ force: true });
 
 module.exports = {
-  sequelize,
-  Address,
-  Category,
-  Food,
-  Menu,
-  OrderDetail,
-  Order,
-  Rating,
-  Restaurant,
-  User,
-  Voucher,
-};
+	sequelize,
+	Address,
+	Category,
+	Food,
+	Menu,
+	OrderDetail,
+	Order,
+	Rating,
+	Restaurant,
+	User,
+	Voucher,
+}
